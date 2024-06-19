edb6c04aad6791f661b251801f2369d55de52ed0
Krasnobaev Roman <romakr007@gmail.com>
2024-06-19 18:40:54 +0300

    Мердж конфликт

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World and some one!");
+        Console.WriteLine("Hello, World and some one! мердж конфликт?");
     }
 }
