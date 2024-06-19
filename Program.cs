ce9667a64f55ae2fd6514bef8837368ae12464c5
Krasnobaev Roman <romakr007@gmail.com>
2024-06-19 18:40:14 +0300

    Для мердж конфиликта

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World and some one!");
+        Console.WriteLine("Hello, World and some one (пытаюсь вызвать мердж конфиликт!");
     }
 }
