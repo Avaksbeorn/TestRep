cc0f265bc0bb5ee9195eca234e9f82ff75914642
Krasnobaev Roman <romakr007@gmail.com>
2024-06-25 11:13:56 +0300

    Решение 1 задачи 6 варианта: "Введите координаты точки на плоскости. Определите где она расположена (на какой оси или в каком координатном углу)."

--- Program.cs
+++ Program.cs
@@ -1,7 +1,48 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World and some one! мердж конфликт?");
+        Console.Write("Введите координату X: ");
+        double x;
+        while (!double.TryParse(Console.ReadLine(), out x))
+        {
+            Console.Write("Некорректный ввод. Введите координату X: ");
+        }
+
+        Console.Write("Введите координату Y: ");
+        double y;
+        while (!double.TryParse(Console.ReadLine(), out y))
+        {
+            Console.Write("Некорректный ввод. Введите координату Y: ");
+        }
+
+        if (x > 0 && y > 0)
+        {
+            System.Console.WriteLine("Точка находится в первом координатном углу.");
+        }
+        else if (x < 0 && y > 0)
+        {
+            System.Console.WriteLine("Точка находится во втором координатном углу.");
+        }
+        else if (x < 0 && y < 0)
+        {
+            System.Console.WriteLine("Точка находится в третьем координатном углу.");
+        }
+        else if (x > 0 && y < 0)
+        {
+            System.Console.WriteLine("Точка находится в четвертом координатном углу.");
+        }
+        else if (x == 0 && y != 0)
+        {
+            System.Console.WriteLine("Точка находится на оси Y.");
+        }
+        else if (y == 0 && x != 0)
+        {
+            System.Console.WriteLine("Точка находится на оси X.");
+        }
+        else
+        {
+            System.Console.WriteLine("Точка находится в начале координат.");
+        }
     }
 }
